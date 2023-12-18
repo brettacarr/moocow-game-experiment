5638627624b0efd91f6e208c65c25d5f2819f95d
brettcarr <brett.a.carr@gmail.com>
2023-12-18 13:54:12 +1100

    Backgroud, fence and collision. Changes Moocow to KinematicBody2D

--- MooCow.gd
+++ MooCow.gd
@@ -1,37 +1,46 @@
-extends Area2D
+extends KinematicBody2D
 
 
-export var speed = 400
+export var speed = 200
 var screen_size
-
+var velocity = Vector2()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
 
-func _process(delta):
-		var velocity = Vector2.ZERO
-		if Input.is_action_pressed("move_right"):
-				velocity.x += 1
-		if Input.is_action_pressed("move_left"):
-				velocity.x -= 1
-		if Input.is_action_pressed("move_down"):
-				velocity.y += 1
-		if Input.is_action_pressed("move_up"):
-				velocity.y -= 1
+func get_input():
+	# Detect up/down/left/right keystate and only move when pressed.
+	
+	velocity = Vector2()
+	if Input.is_action_pressed('ui_right'):
+		velocity.x += 1
+	if Input.is_action_pressed('ui_left'):
+		velocity.x -= 1
+	if Input.is_action_pressed('ui_down'):
+		velocity.y += 1
+	if Input.is_action_pressed('ui_up'):
+		velocity.y -= 1
+	velocity = velocity.normalized() * speed
+	
+func _physics_process(delta):
+	get_input()
+	
+	if velocity.length() >0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite.play()
+	else:
+		$AnimatedSprite.stop()
 		
-		if velocity.length() >0:
-				velocity = velocity.normalized() * speed
-				$AnimatedSprite.play()
-		else:
-				$AnimatedSprite.stop()
-				
-		position += velocity * delta
-		position.x = clamp(position.x, 0, screen_size.x)
-		position.y = clamp(position.y, 0, screen_size.y)
-		
-		if (velocity.x != 0):
-				$AnimatedSprite.animation = "walk"
-				$AnimatedSprite.flip_h = velocity.x > 0
-		if (velocity.y != 0):
-			$AnimatedSprite.animation = "stand"
+	if (velocity.x != 0):
+		$AnimatedSprite.animation = "walk"
+		$AnimatedSprite.flip_h = velocity.x > 0
+	if (velocity.y != 0):
+		$AnimatedSprite.animation = "stand"
+	
+	position += velocity * delta
+	position.x = clamp(position.x, 32, screen_size.x-32)
+	position.y = clamp(position.y, 64, screen_size.y-64)
+	move_and_collide(velocity * delta)
+	
+	
